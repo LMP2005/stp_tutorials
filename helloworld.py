9cdbe733444d4fb9605445f478bced57657e8376
LMP2005 <lisa.preston@postgrad.manchester.ac.uk>
2024-01-01 14:07:38 +0000

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+#W3 schools tutorial 'Hello world' intro
+print("Hello, World!")
+
+# W3 schools Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+# W3 schools Variables
+x = 5
+y = "Hello, World"
+print(x)
+print(y)
+
+# W3 schools Comments #
+# this is a comment and is not code. used to explain code and make it more readable.
+print("Hello, World") # this is a comment
+
+# Multiline string """ (triple quotes), also used in code as comments and comment placed inside it.
+"""
+This also is a comment 
+written in more than one line, 
+this saves time using # at start of each line. 
+"""
+# as long as a string is not assigned a variable, python will read the code but ignore it
