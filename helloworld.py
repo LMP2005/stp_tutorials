9f71106ab6305a5e48d3a41a5e81770cdc961267
LMP2005 <lisa.preston@postgrad.manchester.ac.uk>
2024-01-02 14:21:15 +0000

    w3 schools tutorial variables

--- helloworld.py
+++ helloworld.py
@@ -55,3 +55,21 @@ _my_var = "John"
 myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
+
+# Multiple variables
+x, y, z = "orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#same value to multiple variables
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+#unpacking a list
+fruits = ["apple", "banana", "cherry"]
+x , y, z = fruits
+print(x)
+print(y)
+print(z)
+
